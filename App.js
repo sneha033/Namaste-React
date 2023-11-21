69cb9c884b03ffa1bfee1e4e5f56210159ed444a
sneha033 <gajre.snehal@gmail.com>
2023-11-21 21:33:28 +0530

    3 - LayingTheFoundation

--- App.js
+++ App.js
@@ -1,35 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/*
-<div id="parent">
-    <div id="child1">
-        <h1>I am h1 tag<h1>
-        <h2>I am h2 tag<h2>
-    <div>
-     <div id="child2">
-        <h1>I am h1 tag<h1>
-        <h2>I am h2 tag<h2>
-    <div>
 
-<div>
+const heading = <h1>Hello World from JSX</h1>;
+const multiLineHead = (
+  <h1 className="head" tabName="linee">
+    Hello from multilne JSX
+  </h1>
+);
 
+const reactElement = <h1>Normal JSX element</h1>;
 
-*/
+const FunctionalComponent1 = () => {
+  return <h1>Inside functional component</h1>;
+};
 
-const parent = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")]),
+const FunctionalComponent2 = () => (
+  <h1>Inside functional component single line</h1>
+);
 
-        React.createElement("div", { id: "child2" },
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")])])
+const FunctionalComponent3 = () => (
+  <div id="container">
+    <FunctionalComponent1 />
+    <h1>Inside functional component multi line</h1>
+  </div>
+);
 
+const a = 10000;
 
-const heading = React.createElement("h1", {
-    id: "heading",
-    xyz: "abc"
-}, "Hello World from React!");
+const Title = () =>(
+    <h1>Title New</h1>
+)
+const HeadingComp = () => (
+  <div>
+    {a}
+    {console.log("abc")}
+    <Title/>
+    <Title></Title>
+    {Title()}
+    {reactElement}
+    <h2>Headding</h2>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
-root.render(parent);
+//root.render(reactElement);
+root.render(<HeadingComp />);
